ab0d64e2ac66af5873a2e877712e79eb87cc3bab
MomNoodle <ss313022298@163.com>
2018-05-19 15:56:52 +0800

    2

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+
+
+
+# 添加命名空间(用于区分同为detail的url,所属不同app)
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail')
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/results/', views.results, name='results'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Choice, Question
+from django.urls import reverse
 # Create your views here.
-from .models import Question
+
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # 获取question全部对象，进行排序选取最新5个问题
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    # 将对象以字典形式传输
     context = {'latest_question_list': latest_question_list}
+    # 使用render(request, 模板, 参数)
     return render(request, 'polls/index.html', context)
 
 
-
 def detail(request, question_id):
-    return HttpResponse('this is detail %s'%question_id)
+    # 通过问题id得到question对象
+    question = get_object_or_404(Question, pk=question_id)
+    # 传输给模板
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {'question': question,'error_message': "You didn't select a choice.",})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+def results(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/results.html', {'question': question})
 
 
