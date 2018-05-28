3bc8e1d87c647810bc86c89f363edbf4a5a60684
MomNoodle <ss313022298@163.com>
2018-05-28 16:59:41 +0800

    12

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Question(models.Model):
@@ -8,6 +10,14 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
@@ -15,4 +25,6 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,24 +2,47 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Choice, Question
 from django.urls import reverse
+from django.views.generic import ListView,DetailView
+from django.utils import timezone
 # Create your views here.
 
 
-def index(request):
+# def index(request):
     # 获取question全部对象，进行排序选取最新5个问题
-    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    # latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
     # 将对象以字典形式传输
-    context = {'latest_question_list': latest_question_list}
+    # context = {'latest_question_list': latest_question_list}
     # 使用render(request, 模板, 参数)
-    return render(request, 'polls/index.html', context)
+    # return render(request, 'polls/index.html', context)
 
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
+    # def get_queryset(self):
+    #     return Question.objects.order_by('-pub_date')[:3]
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+# def detail(request, question_id):
     # 通过问题id得到question对象
-    question = get_object_or_404(Question, pk=question_id)
+    # question = get_object_or_404(Question, pk=question_id)
     # 传输给模板
-    return render(request, 'polls/detail.html', {'question': question})
+    # return render(request, 'polls/detail.html', {'question': question})
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 
@@ -41,10 +64,15 @@ def vote(request, question_id):
 
 
 
-def results(request, question_id):
+# def results(request, question_id):
 
-    question = get_object_or_404(Question, pk=question_id)
+    # question = get_object_or_404(Question, pk=question_id)
+    #
+    # return render(request, 'polls/results.html', {'question': question})
+class ResultsView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
-    return render(request, 'polls/results.html', {'question': question})
+
 
 
